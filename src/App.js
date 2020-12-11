eed212c7a3d6441bd2672d5dcea5b35a31cde181
Stacey Graham <stacey.graham1106@gmail.com>
2020-12-10 22:29:23 -0500

    updating App and Landing page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
+
+import Landing from './components/Landing';
 import './index.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <h1>
-          Feel Good Wellness!
-        </h1>
-        <p>Incorporating a healthy lifestyle can really help improve your day and even your week and give you mental clarity.</p>
-        <p>Try incorporating one of these options into your day:</p>
-        <p>Choose one:</p>
-        <ul>
-          <li>Yoga</li>
-          <li>Mediation</li>
-          <li>Smoothies</li>
-          <li>Juicing</li>
-          <li>Meals</li>
-        </ul>
+      <div className="app-container">
+       <Landing />
       </div>
     );
   }
-  
-}
 
 export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Landing = () => {
+        return (
+            <div className="container">
+                <Landing />
+                <h1>
+                    Feel Good Wellness!
+                </h1>
+                <p>Incorporating a healthy lifestyle can really help improve your day and even your week and give you mental clarity.</p>
+                <p>Try incorporating one of these options into your day:</p>
+                <p>Choose one:</p>
+                <ul>
+                    <li>Yoga</li>
+                    <li>Mediation</li>
+                    <li>Smoothies</li>
+                    <li>Juicing</li>
+                    <li>Meals</li>
+                </ul>
+            </div>
+        )
+    }
+
+export default Landing;
