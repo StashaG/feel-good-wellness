d63153252c6e0851592d08080a1f15b185b0f3aa
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-02 09:34:16 -0500

    upating footer component and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,9 @@ import Container from 'react-bootstrap/Container'
 
 const Footer = () => {
     return (
-        // <footer class="footer mt-auto py-3 bg-light">
-        //     <div class="navbar fixed-bottom">
-        //         <span class="text-muted">Feel Good Wellness</span>
-        //     </div>
-        // </footer>
-
-        <div className="fixed-bottom">  
-        <Navbar color="dark" dark>
-            <Container>
-                <Navbar.Brand><span class="text-muted">&copy; Feel Good Wellness</span></Navbar.Brand>
-            </Container>
-        </Navbar>
-    </div>
+        <div className="footer">
+            <span class="lighten-text">&copy; Feel Good Wellness</span>
+        </div>
     );
 }
 
