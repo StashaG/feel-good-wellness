b9a43a393907905f75e8222cc1feda7761f9c4ba
Matthew Day <matt@matthewday.net>
2021-01-26 20:45:18 -0500

    Refactor redux

--- src/redux/reducers/yoga.js
+++ src/redux/reducers/yoga.js
@@ -1,29 +1,27 @@
-import {  
-  ADD_FAVORITE, 
-  REMOVE_FAVORITE, 
-  CLEAR_ITEMS, 
-  HANDLE_YOGA_ITEMS 
+import {
+  CLEAR_ITEMS,
+  HANDLE_YOGA_ITEMS,
+  TOGGLE_FAVORITE,
 } from "../actionTypes";
 
-const initialState =  [];
+const initialState = [];
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case HANDLE_YOGA_ITEMS: 
-      return [
-        ...state,
-        ...action.payload,
-      ]
-      case ADD_FAVORITE:
-        console.log(action.payload.id);
-        const target = state.findIndex(x => x.id === action.payload.id)
-        const items = [...state]
-        items[target].isFavorite = true
-        return items;
-
-      case CLEAR_ITEMS:
-        return [];
-      default: 
-        return state;
-    }
+    case HANDLE_YOGA_ITEMS:
+      return [...state, ...action.payload];
+    case CLEAR_ITEMS:
+      const resetYogaData = state.map((item) => {
+        if (item.isFavorite) item.isFavorite = false;
+        return item;
+      });
+      return resetYogaData;
+    case TOGGLE_FAVORITE:
+      const target = state.findIndex((x) => x.id === action.payload.id);
+      const items = [...state];
+      items[target].isFavorite = !items[target].isFavorite;
+      return items;
+    default:
+      return state;
   }
+}
